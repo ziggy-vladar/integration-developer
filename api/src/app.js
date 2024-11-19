6f3ea633954675e70e53911c47f5bc1c1e9a0b83
Zsigmond Vladar <zsvladar@yettel.hu>
2024-11-19 21:57:08 +0100

    [CLEAN] Remove junk

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const albumsRouter = require('./routes/albums');
 const artistsRouter = require('./routes/artists');
 const genresRouter = require('./routes/genres');
--- api/src/config/config.js
+++ api/src/config/config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: 'music',
-    host: 'mysql',
-    port: 3306,
-    dialect: 'mysql',
-  },
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: 'music',
+  host: 'mysql',
+  port: 3306,
+  dialect: 'mysql',
 };
